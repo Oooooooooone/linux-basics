fa4bf98e332ed5a63527c1516a19a1cffbcac2cd
Oooooooooone <jeongwon8411@gmail.com>
2025-04-16 12:15:42 +0900

    quiz3-10

--- week7/quiz3-10.sh
+++ week7/quiz3-10.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-for i in 1 2 3 4 5
+file_count=0
+dir_count=0
+for item in *
 do
-	echo "현재 숫자는 $i 입니다."
+	if [ -d "$item" ]
+	then 
+			dir_count=$((dir_count+1))
+	elif [ -f "$item" ]
+	then 
+			file_count=$((file_count+1))
+	else
+		echo "$item x"
+	fi
 done
+echo "파일 수 : $file_count"
+echo "디렉토리 수 : $dir_count"
 exit 0
-
